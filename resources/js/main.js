18ed790a7c1dc831e045c29e3830158059a6a251
Simmar Kalsi <simmarkalsi@gmail.com>
2017-09-09 01:35:39 +0530

    JS start

---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if (value) {
+        
+    }
+    console.log(value);
+});
